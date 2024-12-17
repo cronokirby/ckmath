dad6d41623a9297b8da767cb463516f4d8c8932f
Lucas Meier <lucas@cronokirby.com>
2024-12-17 10:17:22 +0100

    Define opposite category

--- CKMath/Category/Elementary.lean
+++ CKMath/Category/Elementary.lean
@@ -4,7 +4,6 @@
 
 import CKMath.Category.Definition
 
-
 namespace Category
 
 /-- A generalization of the category of types to apply to any sort.
@@ -36,3 +35,29 @@ This is our version of `SET` in textbook category theory.
 abbrev ofType := ofSort.{1}
 
 end Category
+
+/-- A carrier for the opposite of a category. -/
+structure Opposite (α) where
+  unop : α
+
+/-- Give a category, explicitly construct the opposite category.
+
+This provides a way to get the canonical instance of the opposite category,
+allowing us to easily use the fact that we've used the standard instance in proving things.
+
+This allows us to lean on opposites when defining categorical constructs.
+-/
+def Category.Op(C : Category O) : Category (Opposite O) := {
+  Mor (A B) := C.Mor B.unop A.unop,
+  id := C.id,
+  comp (f g) := C.comp g f,
+  pre_id := by
+    simp [C.post_id]
+  post_id := by
+    simp [C.pre_id]
+  comp_assoc := by
+    simp [C.comp_assoc]
+}
+
+/-- The canonical instance for the opposite category. -/
+instance instCategoryOpposite [C : Category O] : Category (Opposite O) := C.Op
