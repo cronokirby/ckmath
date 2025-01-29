5dbb0df2f6aab2be9b307881db21c1dc858001db
Lucas Meier <lucas@cronokirby.com>
2025-01-28 18:32:14 -0800

    Remove old category files

--- CKMath.lean
+++ CKMath.lean
@@ -1,6 +1,1 @@
-import CKMath.Category.Basic
-import CKMath.Category.Definition
-import CKMath.Category.Elementary
-import CKMath.Category.Functor
-import CKMath.Category.Morphism
 import CKMath.Category.Test
