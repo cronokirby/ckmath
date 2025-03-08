02c547253e226a215af631b205caea136419aad3
Lucas Meier <lucas@cronokirby.com>
2025-03-08 00:26:36 -0800

    Define hom functor
    This adds the covariant hom functor. We don't define the contravariant
    yet, or intend to, since that can be covered by using the opposite
    category.

--- CKMath.lean
+++ CKMath.lean
@@ -2,3 +2,4 @@ import CKMath.Category.Definition
 import CKMath.Category.Elementary
 import CKMath.Category.Functor
 import CKMath.Category.Iso
+import CKMath.Category.Yoneda
---
+++ CKMath/Category/Yoneda.lean
@@ -0,0 +1,25 @@
+import CKMath.Category.Definition
+import CKMath.Category.Elementary
+import CKMath.Category.Functor
+
+namespace Category
+
+variable [𝓐 : Category (O := OA) A]
+
+/-- Represents the natural hom functor, mapping morphisms to functions between hom-sets. -/
+def Hom (x : OA) : Functor A Fun where
+  obj y := A x y
+  map {a b} (f : A a b) := fun (g : A x a) ↦ g ≫ f
+  map_id := by
+    intros
+    simp only [𝓐.post_id]
+    rfl
+  map_comp := by
+    intros a b f c g
+    funext φ
+    calc
+      _ = (φ ≫ f) ≫ g := by rw [comp_assoc]
+      _ = _ := by trivial
+
+
+end Category
