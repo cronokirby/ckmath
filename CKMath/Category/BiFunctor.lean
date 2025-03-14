214a1a440dadc06a89116836456ad10cd0145dd3
Lucas Meier <lucas@cronokirby.com>
2025-03-14 16:19:53 -0700

    Define BiFunctor

---
+++ CKMath/Category/BiFunctor.lean
@@ -0,0 +1,17 @@
+import CKMath.Category.Definition
+import CKMath.Category.Elementary
+import CKMath.Category.Functor
+
+namespace Category
+
+/-- Represents a bifunctor from two categories to a target one. -/
+def BiFunctor
+  (A : OA → OA → Sort v_A)
+  (B : OB → OB → Sort v_B)
+  (C : OC → OC → Sort v_C)
+  [𝓐 : Category A]
+  [𝓑 : Category B]
+  [𝓒 : Category C] :=
+  Functor (BiMorphism A B) C
+
+end Category
