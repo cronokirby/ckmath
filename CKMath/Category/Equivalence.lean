fb7574418c7f7da15ca269a36d7f9f41e6fa23fc
Lucas Meier <lucas@cronokirby.com>
2025-03-16 17:03:31 -0700

    Define category equivalence

---
+++ CKMath/Category/Equivalence.lean
@@ -0,0 +1,24 @@
+import CKMath.Category.Definition
+import CKMath.Category.Functor
+import CKMath.Category.Iso
+
+namespace Category
+
+/-- Represents an equivalence between two categories.
+
+This shows that the two categories are essentially the same.
+-/
+structure Equivalence
+  (A : OA → OA → Sort v_A)
+  (B : OB → OB → Sort v_B)
+  [Category A]
+  [Category B]
+where
+  fwd : A ⥤ B
+  bwd : B ⥤ A
+  fwd_bwd_iso_id : (fwd ⋙ bwd) ≅ Functor.id
+  bwd_fwd_iso_id : (bwd ⋙ fwd) ≅ Functor.id
+
+infixr:82 " ≅ " => Equivalence
+
+end Category
